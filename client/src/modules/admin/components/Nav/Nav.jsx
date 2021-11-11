75ccb0155481f16aa08b992d233affcbaf51b445
Radosław Łuckoś <radoslaw.luckos@angrynerds.co>
2021-11-11 21:13:24 +0100

    nav fix for admin panel

--- client/src/modules/admin/components/Nav/Nav.jsx
+++ client/src/modules/admin/components/Nav/Nav.jsx
@@ -9,31 +9,31 @@ const Nav = () => {
     const { url } = useRouteMatch();
 
     return (
-        <nav className='menu'>
-            <h4 className='menu__title'>Menu</h4>
-            <ul className='menu__items-list'>
-                <li className='menu__item'>
+        <nav className='nav'>
+            <h4 className='nav__title'>Menu</h4>
+            <ul className='nav__items-list'>
+                <li className='nav__item'>
                     <Link to={`${url}/uzytkownicy`}>
                         <Button variant='contained'>
                             Użytkownicy
                         </Button>
                     </Link>
                 </li>
-                <li className='menu__item'>
+                <li className='nav__item'>
                     <Link to={`${url}/relacje`}>
                         <Button variant='contained'>
                             Relacje
                         </Button>
                     </Link>
                 </li>
-                <li className='menu__item'>
+                <li className='nav__item'>
                     <Link to={`${url}/ogloszenia`}>
                         <Button variant='contained'>
                             Ogłoszenia
                         </Button>
                     </Link>
                 </li>
-                <li className='menu__item'>
+                <li className='nav__item'>
                     <Link to='/admin'>
                         <Button variant='contained'>
                             Twój Profil
