16a24a3864cbf135ed4336e7b3450b2938321ec3
Radosław Łuckoś <radoslaw.luckos@angrynerds.co>
2021-11-20 13:13:34 +0100

    leaflet map applied

--- client/src/modules/static/pages/Contact/Contact.jsx
+++ client/src/modules/static/pages/Contact/Contact.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './Contact.scss';
 
 const Contact = () => {
+
+    const position = [51.0983292, 17.0921347];
+
     return (
-        <section>
-            Contact works!
-        </section>
+        <MapContainer center={position} zoom={15} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    HOW Stanica
+                </Popup>
+            </Marker>
+        </MapContainer>
     )
 }
 
